a8d210aa957f0034c85163d6c930d3c7c7404142
AlinLovin <78381461+AlinLovin@users.noreply.github.com>
2021-03-21 18:55:00 +0200

    Add code

--- Script.js
+++ Script.js
@@ -1,1 +1,9 @@
-//my script
+function addText() {
+	var textVal = document.getElementById('textList').value,
+		listNode = document.getElementById('list'),
+		liNode = document.createElement("LI"),
+		textNode = document.createTextNode(textVal);
+
+	liNode.appendChild(textNode);
+	listNode.appendChild(liNode);
+}
